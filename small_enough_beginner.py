04cb2fa4c3800b69610827b73644cc2818835e38
Sbtha <shisastore@protonmail.com>
2021-04-29 15:38:18 +0200

    Added Small enough? - Beginner Kata solution, as small_enough_beginner.py

---
+++ small_enough_beginner.py
@@ -0,0 +1,9 @@
+# You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+# You can assume all values in the array are numbers.
+
+
+def small_enough(array, limit):
+	return all(number <= limit for number in array)
+
+
+print(small_enough([78, 33, 22, 44, 88, 9, 6], 87))
