d5388ea67aff7a5907ac7e70ff1b5461bb7f4176
Sbtha <shisastore@protonmail.com>
2021-05-23 09:35:22 +0200

    Added docstring to a function

--- mumbling.py
+++ mumbling.py
@@ -6,6 +6,9 @@
 
 def accum(s):
 	"""
+	Returns new string generated from s, where all elements are multiplied by element's position number.
+	Also 1st symbol of new string is capitalized and elements are seperated by dashes.
+	ie: from "abcd" -> A-Bb-Ccc-Dddd"
 	"""
 	new_list = []
 	for i, element in enumerate(s):
