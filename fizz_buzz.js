904f907b04abb250fab7667e0c6569e73e54558c
sbtah <shisastore@protonmail.com>
2021-11-14 15:05:08 +0100

    Added Fizz Buzz challange done in JavaScript

---
+++ fizz_buzz.js
@@ -0,0 +1,17 @@
+// Tried a Fizz Buzz Challange in JavaScript....
+
+function fizzBuzz() {
+    for (i = 1; i < 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('Fizz Buzz!')
+        } else if (i % 5 === 0) {
+            console.log('Buzz')
+        } else if (i % 3 === 0) {
+            console.log('Fizz')
+        } else {
+            console.log(i)
+        }
+    }
+};
+
+fizzBuzz()
