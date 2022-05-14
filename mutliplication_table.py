97eebb94e5f333e54099023239db0120f536b1bb
sbtah <shisastore@protonmail.com>
2022-05-14 09:27:48 +0100

    Added solution for CodeWars Kata:Multiplication Table.

--- mutliplication_table.py
+++ mutliplication_table.py
@@ -7,4 +7,10 @@
 # for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
 
 def multiplication_table(size):
-    return 
+
+    proto = [[x for x in range(1, size +1)] for x in range(1, size +1)]
+    final = [[x * (i + 1) for x in range(1, size + 1)] for i, v in enumerate(proto)]
+
+    return final
+
+print(multiplication_table(4))
